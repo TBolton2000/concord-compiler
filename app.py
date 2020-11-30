317931086b87848abb492e7b944559a5dd27f557
TBolton2000 <TBolton2000@aol.com>
2020-11-30 10:20:32 -0500

    Completed microservice

--- app.py
+++ app.py
@@ -1,45 +1,111 @@
 # app.py
+import multiprocessing as mp
+import sys
+import re
+from io import StringIO
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
+def runCode(code, conn, inputValue, individual):
+    outputStream = StringIO()
+    inputStream = StringIO(inputValue)
+    sys.stdout = outputStream
+    sys.stdin = inputStream
+    if individual:
+        __name__ = "__main__"
+    exec(code)
+    sys.stdout = sys.__stdout__
+    sys.stdin = sys.__stdin__
+    outputString = outputStream.getvalue()
+    print("From child: outputStream = ", outputString)
+    conn.send(outputString)
+    conn.close()
+
+def runCodeHandler(code, inputValue, individual):
+    stringStream = StringIO()
+    parentConn, childConn = mp.Pipe()
+    p = mp.Process(target=runCode, args=(code, childConn, inputValue, individual))
+    p.start()
+    p.join(3)
+    if(p.exitcode == None):
+        # Timeout occured
+        p.terminate()
+        return False, "Timeout occured"
+    else:
+        printedResult = parentConn.recv()
+        return True, printedResult
+
+@app.route('/run-individual/', methods=['POST'])
+def runIndividual():
     # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+    #code = request.form.get("code")
+    code = """
+if __name__ == "__main__":
+    print("Hello world")
+    print(10+21)
+    for i in range(10):
+        print(i)
+    """
+    inputValue = request.form.get("input")
 
     # For debugging
-    print(f"got name {name}")
+    print(f"got code {code}")
+    print(f"got input {inputValue}")
 
     response = {}
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
+    # Check if user sent a code at all
+    if not inputValue:
+        inputValue = ""
+    if not code:
+        response["ERROR"] = "no code found, please send code."
+    # Now the user entered a valid code
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+        success, outputValue = runCodeHandler(code, inputValue, True)
+        if success:
+            response["RESULT"] = outputValue
+        else:
+            response["ERROR"] = outputValue
 
     # Return the response in json format
     return jsonify(response)
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
+@app.route('/run-main/', methods=['POST'])
+def runMain():
+    codes = request.form.get('code')
+    inputValue = request.form.get('input')
+    response = {}
+    if not inputValue:
+        inputValue = ""
+    if not codes:
+        response["ERROR"] = "no code found, please send code."
+        return jsonify(response)
+    mainCode = codes[0]
+    firstLine, _newline, restOfMain = mainCode.partition("\n")
+    candidateCodes = codes[1:]
+    validPattern = r"^\s*import\s*(?:participant(\d+)\s*,\s*)*participant(\d+)\s*$"
+    valid = re.match(validPattern,firstLine)
+    if not valid:
+        response["ERROR"] = "no initial import found on line 1 or malformed, please include initial input on line 1."
+        return jsonify(response)
+    pattern = r"participant(\d+)"
+    matches = re.findall(pattern,firstLine)
+    matches = [int(match) for match in matches]
+    if len(matches) <= len(candidateCodes) and min(matches) > 0 and max(matches) <= len(candidateCodes):
+        # Compile codes together
+        totalCode = ""
+        for match in matches:
+            totalCode += candidateCodes[match-1]
+        totalCode += restOfMain
+        # Run the code
+        success, outputValue = runCodeHandler(totalCode, inputValue, False)
+        if success:
+            response["RESULT"] = outputValue
+        else:
+            response["ERROR"] = outputValue
     else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+        response["ERROR"] = "import found on line 1, but it is malformed."
+    return jsonify(response)
 
 # A welcome message to test our server
 @app.route('/')
@@ -48,4 +114,5 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
+    mp.set_start_method('spawn')
     app.run(threaded=True, port=5000)
